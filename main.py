74c197729e8c94394f53de62735d0b6933b50a55
Gavin Quirk <gavinquirk@gmail.com>
2020-06-15 16:58:24 -0700

    get_log, get_logs, update_log, delete_log

--- main.py
+++ main.py
@@ -9,6 +9,33 @@ def add_log(text, user):
     print("Added log {}".format(log_id))
 
 
-# add_log("This is log one", "Bob")
-# add_log("This is log two", "Joe")
-# add_log("This is log three", "Jane")
+def get_logs():
+    sql = ("SELECT * FROM logs ORDER BY created DESC")
+    cursor.execute(sql)
+    result = cursor.fetchall()
+
+    for row in result:
+        print(row)
+
+
+def get_log(id):
+    sql = ("SELECT * FROM logs WHERE id = %s")
+    cursor.execute(sql, (id,))
+    result = cursor.fetchone()
+
+    for row in result:
+        print(row)
+
+
+def update_log(id, text):
+    sql = ("UPDATE logs SET text = %s WHERE id = %s")
+    cursor.execute(sql, (text, id,))
+    db.commit()
+    print("Log updated")
+
+
+def delete_log(id):
+    sql = ("DELETE FROM logs WHERE id = %s")
+    cursor.execute(sql, (id,))
+    db.commit()
+    print("Log removed")
